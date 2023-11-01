5c5f5e0cfe4a3516b2c50d16f49d895ec9f78caf
PC-159\mori.r <kendou0308@gmail.com>
2023-11-01 15:37:44 +0900

    マイグレーションファイルの追加

---
+++ src/database/migrations/2023_11_01_045918_create_tasks_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('detail')->nullable();
+            $table->boolean('completion_flag');
+            $table->foreignId('game_id')->references('id')->on('games');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
