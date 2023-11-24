235bd2d61635a1ba96a6e0653e8d46f25d123dc1
PC-159\mori.r <kendou0308@gmail.com>
2023-11-24 11:33:26 +0900

    add:一部の修正

--- src/resources/views/game/index.blade.php
+++ src/resources/views/game/index.blade.php
@@ -107,7 +107,7 @@
                                     <div class="modal-dialog">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h1 class="modal-title fs-5" id="exampleModalLabel">削除確認</h1>
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel-{{ $game->id }}">削除確認</h1>
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                     aria-label="Close"></button>
                                             </div>
